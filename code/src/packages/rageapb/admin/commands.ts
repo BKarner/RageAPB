3a5682dba9917693a5318e7e2f8a4910551e207d
Brentjes <jessybesse@hotmail.com>
2022-03-10 21:31:02 +0100

    Added syntax checks

--- code/src/packages/rageapb/admin/commands.ts
+++ code/src/packages/rageapb/admin/commands.ts
@@ -1,6 +1,11 @@
 // Creates a vehicle via admin command.
 function createVehicle(player: PlayerMp, vehId: string) {
-    mp.vehicles.new(mp.joaat(vehId), player.position)
+    if (vehId.trim().length > 0) {
+        player.outputChatBox('<b>Spawning vehicle!</b>')
+        mp.vehicles.new(mp.joaat(vehId), player.position)
+    }
+    else
+        player.outputChatBox(`<b>Command syntax:</b> /aveh [vehicle_name]`);
 }
 mp.events.addCommand('aveh', createVehicle);
 
@@ -8,7 +13,11 @@ mp.events.addCommand('aveh', createVehicle);
 
 // Creates a weapon via admin command.
 function createWeapon(player: PlayerMp, _: string, wepId: string, ammo: string) {
-    console.log(mp.joaat("weapon_"+wepId), Number(ammo));
-    player.giveWeapon(mp.joaat(wepId), Number(ammo))    
+    if (wepId.trim().length > 0) {
+        player.giveWeapon(mp.joaat(wepId), Number(ammo))
+        player.outputChatBox('<b>Spawning weapon!</b>')
+    }
+    else
+        player.outputChatBox(`<b>Command syntax:</b> /awep [weapon_name] [ammo_amount]`);
 }
 mp.events.addCommand('awep', createWeapon);
