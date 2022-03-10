a42d28e1e5159cf751a4d52b3c121df0815773f7
Brentjes <jessybesse@hotmail.com>
2022-03-10 21:38:36 +0100

    Added teleport to position command.
    Teleport to X,Y,Z values.

--- code/src/packages/rageapb/admin/commands.ts
+++ code/src/packages/rageapb/admin/commands.ts
@@ -1,11 +1,11 @@
 // Creates a vehicle via admin command.
 function createVehicle(player: PlayerMp, vehId: string) {
     if (vehId.trim().length > 0) {
-        player.outputChatBox('<b>Spawning vehicle!</b>')
+        player.outputChatBox('Spawning vehicle!')
         mp.vehicles.new(mp.joaat(vehId), player.position)
     }
     else
-        player.outputChatBox(`<b>Command syntax:</b> /aveh [vehicle_name]`);
+        player.outputChatBox(`Command syntax:/aveh [vehicle_name]`);
 }
 mp.events.addCommand('aveh', createVehicle);
 
@@ -15,9 +15,21 @@ mp.events.addCommand('aveh', createVehicle);
 function createWeapon(player: PlayerMp, _: string, wepId: string, ammo: string) {
     if (wepId.trim().length > 0) {
         player.giveWeapon(mp.joaat(wepId), Number(ammo))
-        player.outputChatBox('<b>Spawning weapon!</b>')
+        player.outputChatBox('Spawning weapon!')
     }
     else
-        player.outputChatBox(`<b>Command syntax:</b> /awep [weapon_name] [ammo_amount]`);
+        player.outputChatBox(`Command syntax: /awep [weapon_name] [ammo_amount]`);
 }
 mp.events.addCommand('awep', createWeapon);
+
+
+// Teleport to location via admin command.
+function teleportToLocation(player: PlayerMp, _: string, x: string, y: string, z: string) {
+    if (!isNaN(parseFloat(x)) && !isNaN(parseFloat(y)) && !isNaN(parseFloat(z)))
+        player.position = new mp.Vector3(parseFloat(x),parseFloat(y),parseFloat(z));
+    else
+        player.outputChatBox(`Command syntax: /tp [x] [y] [z]`);
+}
+mp.events.addCommand('tpto', teleportToLocation);
+
+
