f12e9b05a079bd8c227228240d8f9b5484065c0b
Ben Lyon <benlyon1994@gmail.com>
2022-03-10 03:14:24 +0000

    Added player team data class.

--- code/src/client_packages/data/player/index.ts
+++ code/src/client_packages/data/player/index.ts
@@ -1,14 +1,20 @@
 import './events';
 
+import {GetPlayerTeam, SetPlayerTeam} from '../../natives/player';
+import {TEAMS} from '../team/consts';
+import Team from '../team';
+
 /**
  * Handles the Player class as appropriate.
  */
 class Player {
     public static pool: Array<Player> = [];
-    public static local: Player|null = null;
+    public static local: Player;
 
     public readonly isClient: boolean = false;
-    public readonly ragePlayer: PlayerMp|null = null;
+    public readonly ragePlayer: PlayerMp;
+
+    private _team: number;
 
     constructor(ragePlayer: PlayerMp, client: boolean) {
         // Firstly, determine whether or not we're the client.
@@ -17,6 +23,9 @@ class Player {
         // Assign our rage handle so that we can call any rage player stuff.
         this.ragePlayer = ragePlayer;
 
+        // Assign our player to the default team.
+        this._team = TEAMS.PASSIVE;
+
         // Add this to our current player pool.
         Player.pool.push(this);
 
@@ -24,6 +33,37 @@ class Player {
         if (this.isClient) {
             Player.local = this;
         }
+    }
+
+    /**
+     * Get the player's current team.
+     */
+    get team(): number {
+        return this._team;
+    }
+
+    /**
+     * Set the player's team.
+     * @param id
+     */
+    set team(id: number) {
+        const currentTeam = Team.pool[this._team];
+
+        // Remove our player from the team.
+        if (currentTeam && id !== this._team) {
+            currentTeam.remove(this);
+        }
+
+        // Create our team if it doesn't exist.
+        let newTeam = Team.pool[id];
+        if (!newTeam) {
+            newTeam = new Team(id);
+        }
+
+        // Add our team to the new team.
+        this._team = id;
+        SetPlayerTeam(this.ragePlayer, id);
+        newTeam.add(this);
     }
 }
 
