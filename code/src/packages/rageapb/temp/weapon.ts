9121e9cc53ba95ddb5cf7125a197f806416cfc95
Ben Lyon <benlyon1994@gmail.com>
2022-03-14 15:38:28 +0000

    Fixed import placement.

--- code/src/packages/rageapb/temp/weapon.ts
+++ code/src/packages/rageapb/temp/weapon.ts
@@ -1,8 +1,8 @@
+import {WEAPONS} from '../constants/weapons';
+
 /**
  * When a player spawns, ensure they have a pistol and full ammo.
  */
-import {WEAPONS} from '../constants/weapons';
-
 mp.events.add('playerSpawn', (player: PlayerMp) => {
     player.giveWeapon(WEAPONS.PISTOL.hash, WEAPONS.PISTOL.reserveAmmo);
 });
