5114834fb20dc8eb15ed70984ef4a5403376e73b
Ben Lyon <benlyon1994@gmail.com>
2022-03-14 16:13:43 +0000

    Forced player spawn to trigger event.

--- code/src/packages/rageapb/temp/weapon.ts
+++ code/src/packages/rageapb/temp/weapon.ts
@@ -1,6 +1,13 @@
 import {WEAPONS} from '../constants/weapons';
 
 /**
+ * Forcefully spawn the player.
+ */
+mp.events.add('playerJoin', (player: PlayerMp) => {
+    player.spawn(new mp.Vector3(0,0,70));
+});
+
+/**
  * When a player spawns, ensure they have a pistol and full ammo.
  */
 mp.events.add('playerSpawn', (player: PlayerMp) => {
